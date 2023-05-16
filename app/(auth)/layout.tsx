21b9cd9034d67dcc8810b6332d36861c75a325dd
michelbeyrouty <michelbeyrouty@gmail.com>
2023-05-16 16:55:18 -0400

    Setup tailwind and base routes

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,8 +1,19 @@
-export default function Layout({ children }: { children: React.ReactNode }) {
+import "@/styles/global.css";
+import GlassPane from "@/components/GlassPane";
+
+export default function AuthRootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <head />
-      <body>{children}</body>
+      <body className="h-screen w-screen rainbow-mesh p-6">
+        <GlassPane className="w-full h-full flex items-center justify-center">
+          {children}
+        </GlassPane>
+      </body>
     </html>
   );
 }
