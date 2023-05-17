f97792d73c453bcc0bcc75af4dc3cd0fd4968482
michelbeyrouty <michelbeyrouty@gmail.com>
2023-05-17 13:08:28 -0400

    Add JWT authentication

--- pages/api/register.ts
+++ pages/api/register.ts
@@ -1,12 +1,42 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { db } from '@/lib/db';
+import { hashPassword, createJWT } from '@/lib/auth';
+import { serialize } from "cookie";
+
 
 type Data = {
     name: string
 }
 
-export default function handler(
+export default async function register(
     req: NextApiRequest,
     res: NextApiResponse<Data>
 ) {
-    res.status(200).json({ name: 'John Doe' })
+
+    if (req.method === "POST") {
+
+        const user = await db.user.create({
+            data: {
+                email: req.body.email,
+                password: await hashPassword(req.body.password),
+                firstName: req.body.firstName,
+                lastName: req.body.lastName,
+            },
+        });
+
+        const jwt = await createJWT(user);
+        res.setHeader(
+            "Set-Cookie",
+            serialize(process.env.COOKIE_NAME as string, jwt, {
+                httpOnly: true,
+                path: "/",
+                maxAge: 60 * 60 * 24 * 7,
+            })
+        );
+        res.status(201);
+        res.end();
+    } else {
+        res.status(404);
+        res.end();
+    }
 }
--- pages/api/signin.ts
+++ pages/api/signin.ts
@@ -1,12 +1,51 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { db } from '@/lib/db';
+import { comparePasswords, createJWT } from '@/lib/auth';
+import { serialize } from "cookie";
+
 
 type Data = {
     name: string
 }
 
-export default function handler(
+export default async function signin(
     req: NextApiRequest,
     res: NextApiResponse<Data>
 ) {
-    res.status(200).json({ name: 'John Doe' })
+
+    if (req.method === "POST") {
+
+        const user = await db.user.findUnique({
+            where: {
+                email: req.body.email,
+            },
+        });
+
+        if (!user) {
+            res.status(401);
+            res.json({ error: "Invalid login" } as any);
+            return;
+        }
+
+        if (! await comparePasswords(req.body.password, user.password)) {
+            res.status(401);
+            res.json({ error: "Invalid login" } as any);
+            return;
+        }
+
+        const jwt = await createJWT(user);
+        res.setHeader(
+            "Set-Cookie",
+            serialize(process.env.COOKIE_NAME as string, jwt, {
+                httpOnly: true,
+                path: "/",
+                maxAge: 60 * 60 * 24 * 7,
+            })
+        );
+        res.status(201);
+        res.end();
+    } else {
+        res.status(404);
+        res.end();
+    }
 }
