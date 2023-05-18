1fcdd6c34bcae45f1d3e63c57cb893ab72c6f7bb
michelbeyrouty <michelbeyrouty@gmail.com>
2023-05-17 20:36:42 -0400

    Add ProjectsSkeleton

--- components/ProjectsSkeleton.tsx
+++ components/ProjectsSkeleton.tsx
@@ -1,22 +1,15 @@
+import Link from "next/link";
 import Card from "./Card";
+import ProjectCardSkeleton from "./ProjectCardSkeleton";
 
 // To clean up
 
 export default function ProjectsSkeleton() {
   return [1, 2, 3, 4, 5, 6].map((key) => (
-    <Card key={key} className="!px-6 !py-8 w-1/3 p-3">
-      <div className="animate-pulse">
-        <div className="space-y-6 py-1">
-          <div className="h-2 bg-gray-300 rounded"></div>
-          <div className="space-y-3">
-            <div className="grid grid-cols-3 gap-4">
-              <div className="h-2 bg-gray-300 rounded col-span-2"></div>
-              <div className="h-2 bg-gray-300 rounded col-span-1"></div>
-            </div>
-            <div className="h-2 bg-gray-300 rounded"></div>
-          </div>
-        </div>
-      </div>
-    </Card>
+    <div className="w-1/3 p-3" key={key}>
+      <Link href={`/home`}>
+        <ProjectCardSkeleton />
+      </Link>
+    </div>
   ));
 }
